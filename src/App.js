8e40b269f3001e40e42e3acbfddd0befaf17c04e
Davoassassin27 <davosoler@gmail.com>
2022-12-19 02:22:38 -0300

    arreglo de imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
 import "./App.css";
-import Rutas from "./routes/Rutas";
 import React, { useEffect } from "react";
-function App() {
-  const { productos, fetchGetDataCollection } = useFirebase();
-  useEffect(() => {
-    fetchGetDataCollection();
-    //eslint-disable-next-line
-  }, []);
 
-  return (
-    <div className={`App`}>
-      <Rutas data={productos} />
-    </div>
-  );
-}
-
-export default App;
+export default "App";
